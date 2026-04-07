2c3c3430e39569512eaa1ccb6016e7abfcbbcb17
Joshua Morrison <joshuamorrison35@gmail.com>
2026-04-07 12:43:42 +1000

    feat: add support info export to admin sidebar

--- assets/js/custom-admin.js
+++ assets/js/custom-admin.js
@@ -6,4 +6,25 @@ jQuery(function ($) {
     // add form-table class to Encoder Form tables
     $('.eeb-form table').addClass('form-table');
 
+    // Copy support info to clipboard
+    $('#eeb-copy-support-info').on('click', function (e) {
+        e.preventDefault();
+        var $btn = $(this);
+        var text = $btn.data('support-text');
+        var original = $btn.html();
+
+        var onCopied = function () {
+            $btn.text('Copied!');
+            setTimeout(function () { $btn.html(original); }, 2000);
+        };
+
+        if (navigator.clipboard && navigator.clipboard.writeText) {
+            navigator.clipboard.writeText(text).then(onCopied).catch(function () {
+                prompt('Copy this text:', text);
+            });
+        } else {
+            prompt('Copy this text:', text);
+        }
+    });
+
 });
